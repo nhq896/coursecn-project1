6b7b0ef31c3a44263ec5c550ff64539be69d77b9
Nguyen Huu Quy <nhq9689@gmail.com>
2024-09-21 23:41:05 +0700

    add handle error middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 export const app = express();
 
@@ -32,3 +33,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 400;
   next(err);
 });
+
+app.use(ErrorMiddleware);
