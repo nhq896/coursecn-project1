908190b265ad7a9bb2116f64100a4c5ba21cdbed
Nguyen Huu Quy <nhq9689@gmail.com>
2024-09-21 23:36:08 +0700

    database connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,34 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 export const app = express();
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS?.split(",")?.map((item) => item.trim()),
+    credentials: true,
+  })
+);
+
+// testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working!",
+  });
+});
+
+// unknown routes
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found!`) as any;
+  err.statusCode = 400;
+  next(err);
+});
