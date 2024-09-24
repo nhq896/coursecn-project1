14107998b53abe83a7286f58e93424094d549319
Nguyen Huu Quy <nhq9689@gmail.com>
2024-09-25 04:36:52 +0700

    create course controllers

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
 
 export const app = express();
 
@@ -20,7 +21,7 @@ app.use(
   })
 );
 
-app.use("/api/v1", userRouter);
+app.use("/api/v1", userRouter, courseRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
