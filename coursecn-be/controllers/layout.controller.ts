3e31b0e2d4b092205ae87e3d288151f40859c246
nhq896 <nhq9689@gmail.com>
2024-11-15 06:10:57 +0700

    fix theme ui

--- coursecn-be/controllers/layout.controller.ts
+++ coursecn-be/controllers/layout.controller.ts
@@ -5,75 +5,75 @@ import LayoutModel from "../models/layout.model";
 import cloudinary from "cloudinary";
 
 // Tạo layout mới
-// export const createLayout = CatchAsyncError(
-//   async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       const { type } = req.body;
+export const createLayout = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body;
 
-//       const isTypeExist = await LayoutModel.findOne({ type });
-//       if (isTypeExist) {
-//         return next(new ErrorHandler(`${type} đã tồn tại!`, 400));
-//       }
+      const isTypeExist = await LayoutModel.findOne({ type });
+      if (isTypeExist) {
+        return next(new ErrorHandler(`${type} đã tồn tại!`, 400));
+      }
 
-//       // Xử lý tạo Banner
-//       if (type === "Banner") {
-//         const { image, title, subTitle } = req.body;
-//         const myCloud = await cloudinary.v2.uploader.upload(image, {
-//           folder: "layout",
-//         });
-//         const banner = {
-//           type: "Banner",
-//           banner: {
-//             image: {
-//               public_id: myCloud.public_id,
-//               url: myCloud.secure_url,
-//             },
-//             title,
-//             subTitle,
-//           },
-//         };
-//         await LayoutModel.create(banner);
-//       }
+      // Xử lý tạo Banner
+      if (type === "Banner") {
+        const { image, title, subTitle } = req.body;
+        const myCloud = await cloudinary.v2.uploader.upload(image, {
+          folder: "layout",
+        });
+        const banner = {
+          type: "Banner",
+          banner: {
+            image: {
+              public_id: myCloud.public_id,
+              url: myCloud.secure_url,
+            },
+            title,
+            subTitle,
+          },
+        };
+        await LayoutModel.create(banner);
+      }
 
-//       // Xử lý tạo FAQ
-//       if (type === "FAQ") {
-//         const { faq } = req.body;
-//         const faqItems = await Promise.all(
-//           faq.map(async (item: any) => {
-//             return {
-//               question: item.question,
-//               answer: item.answer,
-//             };
-//           })
-//         );
-//         await LayoutModel.create({ type: "FAQ", faq: faqItems });
-//       }
+      // Xử lý tạo FAQ
+      if (type === "FAQ") {
+        const { faq } = req.body;
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer,
+            };
+          })
+        );
+        await LayoutModel.create({ type: "FAQ", faq: faqItems });
+      }
 
-//       // Xử lý tạo Categories
-//       if (type === "Categories") {
-//         const { categories } = req.body;
-//         const categoriesItems = await Promise.all(
-//           categories.map(async (item: any) => {
-//             return {
-//               title: item.title,
-//             };
-//           })
-//         );
-//         await LayoutModel.create({
-//           type: "Categories",
-//           categories: categoriesItems,
-//         });
-//       }
+      // Xử lý tạo Categories
+      if (type === "Categories") {
+        const { categories } = req.body;
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title,
+            };
+          })
+        );
+        await LayoutModel.create({
+          type: "Categories",
+          categories: categoriesItems,
+        });
+      }
 
-//       res.status(200).json({
-//         success: true,
-//         message: "Tạo layout thành công!",
-//       });
-//     } catch (error: any) {
-//       return next(new ErrorHandler(error.message, 500));
-//     }
-//   }
-// );
+      res.status(200).json({
+        success: true,
+        message: "Tạo layout thành công!",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
 
 // Chỉnh sửa layout
 export const editLayout = CatchAsyncError(
